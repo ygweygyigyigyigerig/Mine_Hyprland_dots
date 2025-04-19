b091caa181a0077750c89baf38acc48aa504971d
Simon <192640157+ygweygyigyigyigerig@users.noreply.github.com>
2025-04-19 21:09:11 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -67,7 +67,7 @@ if [[ ! -d "$DOTFILES_DIR" ]]; then
 fi
 
 # Create ~/.config dirs and sync
-declare -a apps=(hypr waybar swaync rofi scripts kitty)
+declare -a apps=(hypr waybar swaync rofi scripts kitty wlogout)
 echo "Syncing dotfiles to ~/.config/..."
 for app in "${apps[@]}"; do
   SRC="$DOTFILES_DIR/$app/"
