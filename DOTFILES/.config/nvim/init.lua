1ffdb583a1414a9e469f3ecbdd1e63ea1d477fa0
ygweygyigyigyigerig <szymon120529@gmail.com>
2025-09-11 21:25:55 +0200

    Improved transparency of nvim, now it doesn't bug out few features.

--- DOTFILES/.config/nvim/init.lua
+++ DOTFILES/.config/nvim/init.lua
@@ -1,10 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.cmd("highlight Normal guibg=NONE")
-vim.cmd("highlight NonText guibg=NONE")
-vim.cmd("highlight NormalNC guibg=NONE")
-vim.cmd("highlight VertSplit guibg=NONE")
-vim.cmd("highlight Pmenu guibg=NONE")
-vim.cmd("set number")
-vim.cmd("set norelativenumber") -- Disable relative line numbers
-vim.cmd("highlight PmenuSel guibg=NONE")
+vim.cmd([[
+  hi Normal guibg=NONE ctermbg=NONE
+  hi NormalNC guibg=NONE ctermbg=NONE
+]])
