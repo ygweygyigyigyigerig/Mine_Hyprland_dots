76f368c4e8cdf44e30903eef8fee34f48ecf7e4a
ygweygyigyigyigerig <szymon120529@gmail.com>
2025-09-14 20:52:25 +0200

    Removal of useless script.

--- DOTFILES/.config/scripts/wallpaper_download_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Your wallpaper URL
-URL="https://images.wallpapersden.com/image/download/starry-landscape-4k-cool-blue-moon_bW5tbG6UmZqaraWkpJRobWllrWdma2U.jpg"
-
-curl -A "Mozilla/5.0" -L -o ~/.wallpaper/Starry_wallpaper.jpg "$URL"
